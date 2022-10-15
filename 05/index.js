fc4dd48ca0e17c2692183f2ac72fc55b1967d67f
Emily Gagne <ceg9498@gmail.com>
2022-10-15 15:38:10 -0400

    Part 3 Complete

--- 05/index.js
+++ 05/index.js
@@ -4,9 +4,10 @@ function a05start(r1, r2) {
 	console.log('r2',r2);
 
 	initXTK(r1);
+	initThreeJS(r2);
 }
 
-function initXTK(r1) {
+function initXTK(container) {
 	console.log('initializing xtk');
 	var r = new X.renderer3D();
 	r.container='r1';
@@ -16,4 +17,49 @@ function initXTK(r1) {
 	r.camera.position = [-100, -100, 200];
 	r.camera.focus = [0, 0, 0];
 	r.render();
+}
+
+function initThreeJS(container) {
+	console.log('initializing Three.js');
+	scene = new THREE.Scene();
+
+	// set up camera
+	let fov = 75;
+	let ratio = container.clientWidth / container.clientHeight;
+	let zNear = 1;
+	let zFar = 10000;
+	camera = new THREE.PerspectiveCamera(fov, ratio, zNear, zFar);
+	camera.position.set( 0, 0, 100);
+
+	// set up renderer
+	renderer = new THREE.WebGLRenderer({antialias:true});
+	renderer.setSize( container.clientWidth, container.clientHeight );
+	container.appendChild(renderer.domElement );
+
+	// set up lighting
+	ambientLight = new THREE.AmbientLight( 0x404040 );
+	scene.add( ambientLight );
+
+	light = new THREE.DirectionalLight( 0xffffff, 5.0);
+	light.position.set( 10, 100, 10 );
+	scene.add(light);
+
+	// Add a cube
+	geometry = new THREE.BoxGeometry( 20, 20, 20);
+	material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+	cube = new THREE.Mesh( geometry, material);
+	scene.add(cube);
+
+	// Trackball
+	controls = new THREE.TrackballControls( camera, container );
+
+	animate();
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	controls.update();
+
+	renderer.render(scene, camera);
 }
