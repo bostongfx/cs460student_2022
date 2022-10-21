e98fff1026ae792ef6aba249bc99dc47e03644b0
Emily Gagne <ceg9498@gmail.com>
2022-10-21 16:28:54 -0400

    Assignment 06 complete.

--- 06/robot.js
+++ 06/robot.js
@@ -14,6 +14,9 @@ class Robot {
 
 		// Create the arms
 		// left side arm
+		// adding a shoulder
+		this.left_shoulder = new THREE.Bone();
+		this.left_shoulder.position.x = -1;
 		this.left_upper_arm = new THREE.Bone();
 		this.left_upper_arm.position.x = -5;
 		this.left_upper_arm.position.y = -5;
@@ -24,11 +27,15 @@ class Robot {
 		this.left_hand.position.x = -3;
 		this.left_hand.position.y = -1;
 
-		this.neck.add(this.left_upper_arm);
+		this.neck.add(this.left_shoulder);
+		this.left_shoulder.add(this.left_upper_arm);
 		this.left_upper_arm.add(this.left_lower_arm);
 		this.left_lower_arm.add(this.left_hand);
 
 		// right side arm
+		// adding a shoulder
+		this.right_shoulder = new THREE.Bone();
+		this.right_shoulder.position.x = 1;
 		this.right_upper_arm = new THREE.Bone();
 		this.right_upper_arm.position.y = -5;
 		this.right_upper_arm.position.x = 5;
@@ -39,7 +46,8 @@ class Robot {
 		this.right_hand.position.x = 3;
 		this.right_hand.position.y = -1;
 
-		this.neck.add(this.right_upper_arm);
+		this.neck.add(this.right_shoulder);
+		this.right_shoulder.add(this.right_upper_arm);
 		this.right_upper_arm.add(this.right_lower_arm);
 		this.right_lower_arm.add(this.right_hand);
 
@@ -86,4 +94,49 @@ class Robot {
 		scene.add(helper);
 	}
 
+	raise_left_arm = function() {
+		this.movement = 'raise left arm';
+	}
+
+	lower_left_arm = function() {
+		this.movement = 'lower left arm';
+	}
+
+	kick = function() {
+		this.movement = 'kick';
+	}
+
+	onAnimate = function() {
+		if (this.movement == 'raise left arm') {
+			// I added the shoulder so that I could move the entire arm
+			// rather than only the lower arm
+			const qStart = this.left_shoulder.quaternion;
+			const qEnd = new THREE.Quaternion();
+			qEnd.setFromAxisAngle(new THREE.Vector3(0, 0, -1), Math.PI *2/3);
+			qStart.slerp(qEnd, 0.1);
+		} else if (this.movement == 'lower left arm') {
+			// I added the shoulder so that I could move the entire arm
+			// rather than only the lower arm
+			const qStart = this.left_shoulder.quaternion;
+			const qEnd = new THREE.Quaternion();
+			qEnd.setFromAxisAngle(new THREE.Vector3(0, 0, 1), 0);
+			qStart.slerp(qEnd, 0.1);
+		} else if (this.movement == 'kick') {
+			// TODO slerping and check once it is done for a backwards slerp
+  		// you can use the identity quaternion for a backwards slerp
+			const qStart = this.right_upper_leg.quaternion;
+			const qEnd = new THREE.Quaternion();
+			qEnd.setFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI/2);
+			qStart.slerp(qEnd, 0.1);
+			// Quaternion.equals() didn't seem to work
+			if(qStart.x.toFixed(2) === qEnd.x.toFixed(2)) {
+				this.movement = 'kick down';
+			}
+		} else if (this.movement == 'kick down') {
+			const qStart = this.right_upper_leg.quaternion;
+			const qEnd = new THREE.Quaternion();
+			qEnd.setFromAxisAngle(new THREE.Vector3(1, 0, 0), 0);
+			qStart.slerp(qEnd, 0.1);
+		}
+	}
 }
